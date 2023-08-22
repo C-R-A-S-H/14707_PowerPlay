363d50294fd583daaa74b78cb96054d6f64b6a6b
ZeroLogic14707 <127230212+ZeroLogic14707@users.noreply.github.com>
2023-08-22 12:58:53 -0500

    new
    new

---
+++ teamcode2/Management/Resourcemanager.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.Management;
+
+
+public class Resourcemanager {
+
+	// todo: write your code here
+}
---
+++ teamcode2/sensors/Sensors.java
@@ -0,0 +1,10 @@
+// package org.firstinspires.ftc.teamcode.sensors;
+
+// import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+// @TeleOp
+
+// public class Sensors {
+
+//     // todo: write your code here
+// }
